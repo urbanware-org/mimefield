4db3da21dcc7d2db4062dbc4f03ccf8ae0858342
urbanware-org <github@urbanware.org>
2021-11-08 18:41:07 +0100

    Add separate method to get MIME type from a file

--- core/main.py
+++ core/main.py
@@ -15,11 +15,44 @@ import magic
 import os
 import sys
 import shutil
+import subprocess
 
 
 def get_method():
     file_util = shutil.which("filed")
     return file_util is not None
+
+
+def get_mime_type(path, use_magic=False):
+    """
+        Get the MIME type of a file (either using the 'file' utility or
+        'libmagic' module).
+    """
+    if not os.path.isfile(path):
+        return None
+
+    # If not explicitly changed via command-line argument, MIMEfield will read
+    # out the MIME information using the 'file' tool, which is included in all
+    # Linux and Unix-like operating systems by default. In case it is missing,
+    # MIMEfield uses the 'libmagic' file type identification library instead
+    # as fallback.
+    #
+    # However, the 'file' tool does not exist on Windows operating systems, so
+    # MIMEfield will directly use the 'libmagic' library there.
+    if use_magic:
+        m = magic.open(magic.MAGIC_NONE)
+        m.load()
+        ftype = m.file(path).lower()
+    else:
+        p = subprocess.Popen(['file', '--brief', '--mime-type', path],
+                             stdout=subprocess.PIPE)
+        stdout, stderr = p.communicate()
+        ftype = stdout.decode("utf-8").replace("\n", "")
+
+    if ftype == "empty":
+        ftype = ""
+
+    return ftype
 
 
 def get_mime_types(directory, extension, mimetype, verbose=False):
